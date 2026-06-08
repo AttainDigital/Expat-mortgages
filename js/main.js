4416522af96f4fe8d79ecd2f0af361d565d52712
Tomspink <tomspink3@gmail.com>
2026-06-08 23:16:37 +0100

    Cache-bust CSS; smaller logo scale 0.45; 3 logos on mobile; both consent checkboxes confirmed

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 4 } },
       { breakpoint: 768,  settings: { slidesToShow: 3 } },
-      { breakpoint: 480,  settings: { slidesToShow: 2 } }
+      { breakpoint: 480,  settings: { slidesToShow: 3 } }
     ]
   });
 
